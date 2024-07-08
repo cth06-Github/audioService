9306a92a91d4b9fc463a2d55c5d722252a13c2c7
Chua Tse Hui <htcylsg@gmail.com>
2024-07-08 18:30:51 +0800

    authentication revised (halfway)

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,19 +1,32 @@
 import { NextResponse, NextRequest } from "next/server";
+import { getSession } from "./app/lib.ts";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const securePaths = ["/home", "/upload", "/record"];
 
   // Unauthorized access
-  if (secureRouteChecker(pathname, securePaths) && !request.cookies.get("session")) { // need to change
-    // pathname != web URL but project file names
-    return new Response("Error 401: Unauthorized Access", { status: 401 });
+  if (secureRouteChecker(pathname, securePaths)) {
+    if (!request.cookies.get("session")) {
+      return new Response("Error 401: Unauthorized Access", { status: 401 });
+    }
+
+    try {
+      // force generate cookie. Not sure if...there's a certain format for JWT hackers can emulate.
+      await getSession();
+    } catch {
+      return new Response("Error 401: Unauthorized Access", { status: 401 });
+    }
   }
 
-  // User is logged in but access login page without logging out --> redirect to /upload
-  
-  if (pathname === "/login" && request.cookies.get("session")) { /// nened to change
-    return NextResponse.redirect(new URL("/home", request.url));
+  // User is logged in but access login page without logging out --> redirect to /home
+  try {
+    if (pathname === "/login" && request.cookies.get("session")) {
+      await getSession(); // if can, means valid user // WAIT NO IT CANNOT READ NULL VALUES.
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+  } catch {
+    return new Response("please delete cookies and login the proper way"); //NextResponse.redirect(new URL("/login", request.url)); // redirect to itself...consequently users need to delete cookies
   }
 
   return NextResponse.next(); // always written like this hmm
@@ -25,5 +38,7 @@ export const config = {
 };
 
 function secureRouteChecker(pathname: string, pathArray: string[]) {
-  return pathArray.filter(path => path === pathname).length !== 0 ? true : false;
+  return pathArray.filter((path) => path === pathname).length !== 0
+    ? true
+    : false;
 }
