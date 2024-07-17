7c5d32a9d74b5ce6c1d14b9104c812c0d7f96589
Chua Tse Hui <htcylsg@gmail.com>
2024-07-17 11:26:12 +0800

    sideMenu animation

--- frontend/src/app/(components)/header.tsx
+++ frontend/src/app/(components)/header.tsx
@@ -24,38 +24,63 @@ export const Header: React.FC<HeaderProps>  = ({
   onClickFuncHome,
   onClickFuncLogout
 }: HeaderProps): JSX.Element => {
-  const [menu, toggleMenu] = useState<boolean>(false);
+  const [menu, setMenu] = useState<boolean>(false);
+
+  const handleToggleMenu = () => {
+    setMenu(!menu);
+  }
 
   return (
   <div style ={{alignItems: "flex-end", justifyContent: "flex-start"}}>
     <div className={styles.top}>
       <header>
+        <div className={styles.leftHead}>
         <span className={styles.menu}>
-          <button onClick={() => {toggleMenu(!menu)}}>
+          <button onClick={handleToggleMenu}>
           <MenuIcon></MenuIcon>
           </button>
         </span>
         <span className={styles.home}>
-          {hasHome && <HomeButton onClickFunc = {onClickFuncHome}/>}
+          
+          {hasHome && <span><HomeButton onClickFunc = {onClickFuncHome}/></span>}
+          <span>
           <p style={{ display: "flex", alignItems: "center" }}>
             <PersonIcon />
             {user}
           </p>
+          </span>
         </span>
+        </div>
+        <div className={styles.centerHead}>
         <h1>{heading}</h1>
-        <span className={styles.login}>
-          <Logout onClickFunc = {onClickFuncLogout}/>
-        </span>
         <h3>{description}</h3>
+        </div>
+        <div className={styles.rightHead}>
+          <span className={styles.logout}>
+          <Logout onClickFunc = {onClickFuncLogout}/>
+          </span>
+        </div>
       </header>
       </div>
 
-      {menu &&
-      <div className={styles.menuOpen}>
+      
+      <div id = "menu" className={[styles.menuOpen, "menuOpenAlt"].join(" ")}  >
+       <style jsx>{`
+        .menuOpenAlt {
+          width: ${menu ? "90vw" : "0px"};
+          padding: ${menu ? "0px 10vw" : "0px"};
+          box-shadow: ${menu && "70px 0px 0px 10vw rgba(0, 0, 0, 0.5)" /*is this a good way*/
+        }
+      `}</style>
+
+{menu &&
+<>
+<header>
       <h6>
         <PersonIcon />
         {user}
       </h6>
+      </header>
       <ul>
       {hasHome && <HomeButton onClickFunc = {onClickFuncHome}/>}
       </ul>
@@ -64,8 +89,10 @@ export const Header: React.FC<HeaderProps>  = ({
         <Logout onClickFunc = {onClickFuncLogout}/>
       </span>
       </ul>
-    </div>
+      </>
       }
+    </div>
+      
     </div>
     
   );
